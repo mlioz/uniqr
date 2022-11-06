350b07d4e48e2cae4ce42fd4c02474430a975a34
Myron Lioz <liozmyron@gmail.com>
2022-11-06 08:13:55 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use std::error::Error;
+
+use clap::{App, Arg};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    in_file: String,
+    out_file: Option<String>,
+    count: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("uniqr")
+        .about("Rust uniq")
+        .author("Myron Lioz <liozmyron@gmail.com")
+        .version("0.1.0")
+        .arg(
+            Arg::with_name("in_file")
+                .value_name("IN_FILE")
+                .takes_value(true)
+                .help("Input file")
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("out_file")
+                .value_name("OUT_FILE")
+                .help("Output file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("count")
+                .short("c")
+                .long("long")
+                .takes_value(false)
+                .help("prefix lines by the number of occurrences"),
+        )
+        .get_matches();
+
+    Ok(Config {
+        in_file: matches.value_of_lossy("in_file").unwrap().to_string(),
+        out_file: matches.value_of("out_file").map(String::from),
+        count: matches.is_present("count"),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
